9d9a17a176557d8993a465753ffc6bc3592db4d2
Ha Manh Do <v.hadm7@vinai.io>
2022-04-13 16:02:49 +0700

    update schema for unique email address

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import * as argon from 'argon2';
 import { AuthDTO } from './dto';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 @Injectable()
 export class AuthService {
   constructor(private prismaService: PrismaService) {}
@@ -14,21 +15,30 @@ export class AuthService {
   async signup(dto: AuthDTO) {
     // generate the password
     const hash = await argon.hash(dto.password);
-    // save the new user in the db
-    const user = await this.prismaService.user.create({
-      data: {
-        email: dto.email,
-        hash,
-      },
-      // only allow some specific properties returned instead of return all fields
-      // select: {
-      //   id: true,
-      //   email: true,
-      //   createdAt: true,
-      // },
-    });
+    try {
+      // save the new user in the db
+      const user = await this.prismaService.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+        // only allow some specific properties returned instead of return all fields
+        // select: {
+        //   id: true,
+        //   email: true,
+        //   createdAt: true,
+        // },
+      });
 
-    // return the saved user
-    return user;
+      // return the saved user
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      }
+      throw error;
+    }
   }
 }
