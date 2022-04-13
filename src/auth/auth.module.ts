a60ee5ea8e34f3ebbd4af97b88eaa00860065ee0
Ha Manh Do <v.hadm7@vinai.io>
2022-04-13 17:03:22 +0700

    add JwtStrategy and add user controller

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtStategy } from './strategy';
 
 @Module({
   imports: [JwtModule.register({})],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStategy],
 })
 export class AuthModule {}
