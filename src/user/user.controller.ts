fd0f9eb729c9ee52b077960282dc49b4b721a56c
Ha Manh Do <v.hadm7@vinai.io>
2022-04-13 17:29:53 +0700

    custom http status returned

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,10 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  UseGuards,
+} from '@nestjs/common';
 import { User } from '@prisma/client';
 import { GetUser } from 'src/auth/decorator';
 import { JwtGuard } from 'src/auth/guard';
@@ -7,6 +13,7 @@ import { JwtGuard } from 'src/auth/guard';
 @Controller('users')
 export class UserController {
   @Get('me')
+  @HttpCode(HttpStatus.OK)
   // getMe(@GetUser('email') user: User) {
   getMe(@GetUser() user: User) {
     return user;
