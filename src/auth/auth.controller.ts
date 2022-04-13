44fd99610d3deb002f10101dbd96199747f831c5
Ha Manh Do <v.hadm7@vinai.io>
2022-04-13 15:08:19 +0700

    direct using body field inside function parameters

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,21 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, ParseIntPipe, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { AuthDTO } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signup(@Body() dto: AuthDTO) {
-    console.log(dto);
+  signup(
+    @Body('email') email: string,
+    @Body('password', ParseIntPipe) password: string,
+  ) {
+    console.log({
+      email,
+      typeofEmail: typeof email,
+      password,
+      typeofPassword: typeof password,
+    });
     return this.authService.signup();
   }
 
