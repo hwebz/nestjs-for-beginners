db61c1e844be864a2431f644d5350c80f8bcf86f
Ha Manh Do <v.hadm7@vinai.io>
2022-04-13 15:52:10 +0700

    hashing password + save user to db

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
-
+import * as argon from 'argon2';
+import { AuthDTO } from './dto';
 @Injectable()
 export class AuthService {
   constructor(private prismaService: PrismaService) {}
@@ -10,9 +11,24 @@ export class AuthService {
     };
   }
 
-  signup() {
-    return {
-      message: 'Hello Sign Up',
-    };
+  async signup(dto: AuthDTO) {
+    // generate the password
+    const hash = await argon.hash(dto.password);
+    // save the new user in the db
+    const user = await this.prismaService.user.create({
+      data: {
+        email: dto.email,
+        hash,
+      },
+      // only allow some specific properties returned instead of return all fields
+      // select: {
+      //   id: true,
+      //   email: true,
+      //   createdAt: true,
+      // },
+    });
+
+    // return the saved user
+    return user;
   }
 }
