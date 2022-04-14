bd67fa533970d3a76eba80f81e61f9d9950c9af3
Ha Manh Do <v.hadm7@vinai.io>
2022-04-14 11:24:28 +0700

    clean db before running tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,10 +1,11 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
+import { PrismaService } from '../src/prisma/prisma.service';
 import { AppModule } from '../src/app.module';
 
 describe('App e2w', () => {
   let app: INestApplication;
-
+  let prisma: PrismaService;
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
@@ -16,6 +17,9 @@ describe('App e2w', () => {
       }),
     );
     await app.init();
+
+    prisma = app.get(PrismaService);
+    await prisma.cleanDb();
   });
 
   afterAll(() => {
